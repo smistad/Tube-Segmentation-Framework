66f366892f266bd4e666d7fd0c9432d0e1d24f52
Janne Beate Lervik Bakeng <janne.beate.lervik.bakeng@sintef.no>
2012-12-12 15:42:08 +0100

    Project now links both static and shared. Tube-Segmentation-Framework, SIPL and OCL_Utilities can all now be imported to other projects using find_package in CMake.

--- CMake/Tube-Segmentation-FrameworkUse.cmake
+++ CMake/Tube-Segmentation-FrameworkUse.cmake
@@ -1,11 +1,26 @@
-include (CMakeImportBuildSettings)
-cmake_import_build_settings (${Tube-Segmentation-Framework_BUILD_SETTINGS_FILE})
+###########################################################
+##          Tube-Segmentation-Framework Use file
+###########################################################
 
+#------------------------------------------------------------------------------
+# External libraries
+#------------------------------------------------------------------------------
+# Boost
 find_package(Boost REQUIRED)
 
+# GTK
 find_package (PkgConfig REQUIRED)
 pkg_check_modules (GTK2 REQUIRED gtk+-2.0 gthread-2.0)
 
+# SIPL
+find_package(SIPL PATHS "${Tube-Segmentation-Framework_BINARY_DIR}/SIPL" REQUIRED)
+
+# OpenCLUtilities
+find_package(OCL-Utilities PATHS "${Tube-Segmentation-Framework_BINARY_DIR}/OpenCLUtilities" REQUIRED)
+
+#------------------------------------------------------------------------------
+# Where to look for includes and libraries
+#------------------------------------------------------------------------------
 include_directories( ${Tube-Segmentation-Framework_INCLUDE_DIRS}  ${Tube-Segmentation-Framework_BINARY_DIR} ${Boost_INCLUDE_DIRS} ${GTK2_INCLUDE_DIRS})
-link_directories (${Tube-Segmentation-Framework_LIBRARY_DIRS})
+link_directories (${Tube-Segmentation-Framework_LIBRARY_DIRS} ${OCL-Utilities_LIBRARY_DIRS})
 
