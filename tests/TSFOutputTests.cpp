68d81b870fdead933e52e212aea9a0a9a2534de5
Erik Smistad <smistad@idi.ntnu.no>
2013-01-31 16:23:49 +0100

    added get host data test

--- tests/TSFOutputTests.cpp
+++ tests/TSFOutputTests.cpp
@@ -23,6 +23,36 @@ TEST(TSFOutputTest, SetHostData) {
 	ASSERT_EQ(output.hasCenterlineVoxels(), true);
 }
 
+TEST(TSFOutputTest, GetHostData) {
+	TSFOutput output(new OpenCL, new SIPL::int3);
+	float * TDF = new float[3];
+	TDF[0] = 0.5f;
+	TDF[1] = 1.0f;
+	TDF[2] = 0.0f;
+	output.setTDF(TDF);
+	ASSERT_EQ(output.getTDF()[0], 0.5f);
+	ASSERT_EQ(output.getTDF()[1], 1.0f);
+	ASSERT_EQ(output.getTDF()[2], 0.0f);
+
+	char * data = new char[3];
+	data[0] = 1;
+	data[1] = 2;
+	data[2] = 100;
+	output.setSegmentation(data);
+	ASSERT_EQ(output.getSegmentation()[0], 1);
+	ASSERT_EQ(output.getSegmentation()[1], 2);
+	ASSERT_EQ(output.getSegmentation()[2], 100);
+
+	char * data2 = new char[3];
+	data2[0] = 10;
+	data2[1] = 20;
+	data2[2] = 10;
+	output.setCenterlineVoxels(data2);
+	ASSERT_EQ(output.getCenterlineVoxels()[0], 10);
+	ASSERT_EQ(output.getCenterlineVoxels()[1], 20);
+	ASSERT_EQ(output.getCenterlineVoxels()[2], 10);
+}
+
 TEST(TSFOutputTest, GetSize) {
 	SIPL::int3 * size = new SIPL::int3(100, 20, 1);
 	TSFOutput output(new OpenCL, size);
