79e2ce352310b68c3879f88e3d148e4bc378c6f3
Erik Smistad <smistad@idi.ntnu.no>
2013-02-01 14:17:13 +0100

    added numeric parameter validation test

--- tests/parameterTests.cpp
+++ tests/parameterTests.cpp
@@ -1,4 +1,5 @@
 #include "tests.hpp"
+#include "../SIPL/Exceptions.hpp"
 
 // Tests for the parameter system
 
@@ -19,4 +20,14 @@ TEST(ParameterTest, SetParameters) {
 	EXPECT_EQ("lung", getParamStr(parameters, "cropping"));
 	parameters = setParameter(parameters, "tdf-high", "0.9");
 	EXPECT_EQ(0.9f, getParam(parameters, "tdf-high"));
+}
+
+TEST(ParameterTest, NumericParameterValidation) {
+	NumericParameter p;
+	ASSERT_NO_THROW(p = NumericParameter(0.2, 0.1, 1.0, 0.1, "asd"));
+
+	EXPECT_THROW(p.set(0.35), SIPL::SIPLException);
+	EXPECT_THROW(p.set(0.05), SIPL::SIPLException);
+	EXPECT_THROW(p.set(1.2), SIPL::SIPLException);
+	EXPECT_NO_THROW(p.set(0.4));
 }
