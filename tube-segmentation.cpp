58e31314fc0190bde7b24d29588c1edec0a1b379
Erik Smistad <smistad@idi.ntnu.no>
2013-05-13 11:04:08 +0200

    created garbage collector object

--- tube-segmentation.cpp
+++ tube-segmentation.cpp
@@ -5238,4 +5238,22 @@ void TSFOutput::setSpacing(SIPL::float3 spacing) {
 	this->spacing = spacing;
 }
 
+void TSFGarbageCollector::addMemObject(cl::Memory* mem) {
+    memObjects.insert(mem);
+}
 
+void TSFGarbageCollector::deleteMemObject(cl::Memory* mem) {
+    memObjects.erase(mem);
+    delete mem;
+    mem = NULL;
+}
+
+void TSFGarbageCollector::deleteAllMemObjects() {
+    std::set<cl::Memory *>::iterator it;
+    for(it = memObjects.begin(); it != memObjects.end(); it++) {
+        cl::Memory * mem = *it;
+        delete (mem);
+        mem = NULL;
+    }
+    memObjects.clear();
+}
