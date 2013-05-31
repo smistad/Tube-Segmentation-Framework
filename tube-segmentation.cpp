5f86ffecea71aa4f1129feb62bb8ad6e58f00dd6
Erik Smistad <smistad@idi.ntnu.no>
2013-05-31 11:09:21 +0200

    added runSplineTDF function

--- tube-segmentation.cpp
+++ tube-segmentation.cpp
@@ -1464,6 +1464,58 @@ Image3D runGVF(OpenCL &ocl, Image3D * vectorField, paramList &parameters, SIPL::
 	}
 }
 
+void runSplineTDF(
+        OpenCL &ocl,
+        Image3D *vectorField,
+        paramList &parameters,
+        SIPL::int3 &size,
+        Buffer *TDF,
+        Buffer *radius,
+        float radiusMin,
+        float radiusMax,
+        float radiusStep
+    ) {
+
+    // Create blending functions
+    int samples = 3;
+    float s = 0.5;
+    float * blendingFunctions = new float[4*samples]; // 4 * samples per arm
+    for(int i = 0; i < samples; i++) {
+        float u = (float)i / (samples-1);
+        blendingFunctions[i*4] = -s*u*u*u + 2*s*u*u - s*u;
+        blendingFunctions[i*4+1] = (2-s)*u*u*u + (s-3)*u*u + 1;
+        blendingFunctions[i*4+2] = (s-2)*u*u*u + (3-2*s)*u*u + s*u;
+        blendingFunctions[i*4+3] = s*u*u*u - s*u*u;
+    }
+
+    // Transfer to device
+    Buffer bufferBlendingFunctions = Buffer(
+     ocl.context,
+     CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
+     sizeof(float)*samples*4,
+     blendingFunctions
+    );
+
+    Kernel TDFKernel(ocl.program, "splineTDF");
+    TDFKernel.setArg(0, vectorField);
+    TDFKernel.setArg(1, TDF);
+    TDFKernel.setArg(2, std::max(1.0f, radiusMin));
+    TDFKernel.setArg(3, radiusMax);
+    TDFKernel.setArg(4, radiusStep);
+    TDFKernel.setArg(5, bufferBlendingFunctions);
+    TDFKernel.setArg(6, 8); // arms
+    TDFKernel.setArg(7, samples); // samples per arm
+    TDFKernel.setArg(8, radius);
+    TDFKernel.setArg(9, 0.0001f);
+
+    ocl.queue.enqueueNDRangeKernel(
+            TDFKernel,
+            NullRange,
+            NDRange(size.x,size.y,size.z),
+            NDRange(4,4,4)
+    );
+}
+
 void runCircleFittingTDF(OpenCL &ocl, int3 &size, Image3D * vectorField, Buffer * TDF, Buffer * radius, float radiusMin, float radiusMax, float radiusStep) {
     Kernel circleFittingTDFKernel(ocl.program, "circleFittingTDF");
     circleFittingTDFKernel.setArg(0, *vectorField);
